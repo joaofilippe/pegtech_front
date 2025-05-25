188c974a429a4ac8784c74e280f15455c398a22d
Joao Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2025-05-25 17:19:58 -0300

    feat: add ClientProvider to main app for client state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pegtech_front/providers/client_provider.dart';
 import 'package:provider/provider.dart';
 import 'providers/auth_provider.dart';
 import 'pages/login_page.dart';
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => ClientProvider()),
       ],
       child: MaterialApp(
         title: 'PegTech',
